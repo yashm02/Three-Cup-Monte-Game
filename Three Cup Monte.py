7a91c5cb59ec2a41c35f9882d2ab7fb54bd7f0b8
yashm02 <102669147+yashm02@users.noreply.github.com>
2022-11-20 22:29:36 +0530

    Add files via upload

---
+++ Three Cup Monte.py
@@ -0,0 +1,33 @@
+
+from random import shuffle
+
+#initial list
+my_list=[' ','O',' ']
+
+#shuffle list
+def shuffle_list(my_list):
+    shuffle(my_list)
+    return my_list
+
+mixed_list=shuffle_list(my_list)
+
+#player guess
+def player_guess():
+    guess=''
+    while guess not in ['0','1','2']:
+        guess=input("PICK A NUMBER FROM 0, 1 or 2: ")
+
+    return int(guess)
+
+guess=player_guess()
+
+# check guess
+def check_guess(my_list,guess):
+    if(my_list[guess]=='O'):
+        print("CONGRATULATIONS!!! YOU ARE RIGHT")
+    else:
+        print("OOPS!!! WRONG GUESS")
+        print("TRY AGAIN")
+        print(my_list)
+
+check_guess(mixed_list,guess)
